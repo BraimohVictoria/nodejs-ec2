3874beeb60be0742da32601fd721dfa4f0eab850
Victoria <ikpemosi.braimoh@gmail.com>
2024-10-14 15:29:20 +0100

    CloudPlexo'nodejs app

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('CloudPlexo for startups is not an accelerator or vc backed programme.'); //write a response to the client
+  res.write('CloudPlexo offers startups up to 5000 worth of AWS credits.'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 3000

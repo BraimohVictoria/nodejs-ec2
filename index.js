7b7f33d2a2880581aefcbc1a24b2906715a4dbf3
Victoria <ikpemosi.braimoh@gmail.com>
2024-10-14 15:14:03 +0100

    CloudPlexo'nodejs app

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('** Modernise and secure all processes with CloudPlexo!! **'); //write a response to the client
+  res.write('CloudPlexo for startups is not an accelerator or vc backed programmed.'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80

7a777f35a7a3d9bd38db0db3da3cf2b97549c4a0
victoria-cloudplexo <victoria@cloudplexo.com>
2025-01-13 11:41:27 +0100

    Hello and welcome to CloudPlexo

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('CloudPlexo offers startups up to 5000 worth of AWS credits.'); //write a response to the client
+  res.write('Welcome to CloudPlexo.'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 3000
